19ace04b838a7fc2208f2dea9d8f8dd37bcb62e3
DaoXuanTri93 <102907569+DaoXuanTri93@users.noreply.github.com>
2022-04-10 00:19:17 +0700

    file

---
+++ JAVASCRIPT/thucHanhKhaiBaoBien/baitap.js
@@ -0,0 +1,33 @@
+
+
+let i,b,s;
+i = parseInt(10);
+f = 20.5;
+b = true;
+s = "ha noi"
+document.write('i = '+ i )
+document.write('<br>')
+document.write('f = '+ f )
+document.write('<br>')
+document.write('b = '+ b )
+document.write('<br>')
+document.write('s = '+ s )
+document.write('<br>')
+ 
+//  bai tap 2
+
+let width = 20;
+let height = 104;
+let area = width + height
+document.write('Area = ' + area )
+document.write('<br>1')
+// bai tap 3
+
+let a = parseInt(prompt("nhap so a"));
+let d = parseInt(prompt('nhap so d898'));
+
+if (a % d === 0) {
+    alert('d la boi so cua a');
+} else {
+    alert('d khong fai la bo so cua a');
+}
