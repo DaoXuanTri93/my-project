b3ae76916002af36efb6de9fbbbc14f2e5714ae3
DaoXuanTri93 <102907569+DaoXuanTri93@users.noreply.github.com>
2022-04-10 14:06:26 +0700

    file

---
+++ JAVASCRIPT/chuyenDoiTien/main.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+function showMessage() {
+    let a = document.getElementById('number').value;
+    let Result = a * 23000;
+    document.write('Result : ' + Result + 'USD');
+    // console.log(Result);
+}
---
+++ JAVASCRIPT/test.js
@@ -0,0 +1,22 @@
+
+function sum(a,b) {
+    return a.toString() + b.toString();
+}
+var tong = sum(30,2);
+console.log(tong);
+
+
+// var isConfirm = confirm('hello word');
+// console.log(isConfirm);
+
+
+        //  test Arguments
+// function showMessage() {
+//     var myString = '';
+//     for(var a of arguments){
+//     myString = myString + a + '-'
+//     }
+//     console.log(myString);
+// }
+
+// showMessage('test1','test2',1);
