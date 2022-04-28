758cede3c06bc9345e350c52fd414003c5117c5b
DaoXuanTri93 <102907569+DaoXuanTri93@users.noreply.github.com>
2022-04-28 23:28:17 +0700

    file PC

---
+++ FORM/Validator.js
@@ -0,0 +1,15 @@
+
+// Đối tượng contructor
+function Validator(options){
+    // let formElement = document.querySelector(options.value)
+    // console.log(formElement);
+}
+
+
+//Định nghĩa các rulers
+Validator.isRequired = function(){
+
+}
+Validator.isEmail = function(){
+
+}
